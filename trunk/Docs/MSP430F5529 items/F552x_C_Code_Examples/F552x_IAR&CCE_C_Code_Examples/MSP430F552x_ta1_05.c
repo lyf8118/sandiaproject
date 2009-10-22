a29f0f6fa2e08bbaa895212dc4c7f7e527d77cb0
c.shirts <c.shirts@291c8d24-a182-11de-89ee-f3da9a46d971>
2009-10-22 17:21:10 +0000

    Add first of new processor files.

---
+++ trunk/Docs/MSP430F5529 items/F552x_C_Code_Examples/F552x_IAR&CCE_C_Code_Examples/MSP430F552x_ta1_05.c
@@ -0,0 +1,44 @@
+//******************************************************************************
+//  MSP430F552x Demo - Timer1_A3, Toggle P1.0, CCR0 Up Mode ISR, 32kHz ACLK
+//
+//  Description: Toggle P1.0 using software and the TA_1 ISR. Timer1_A is
+//  configured for up mode, thus the timer overflows when TAR counts
+//  to CCR0. In this example, CCR0 is loaded with 1000-1.
+//  Toggle rate = 32768/(1000) = 8.2Hz
+//  ACLK = TACLK = 32768Hz, MCLK = SMCLK = default DCO ~1.045MHz
+//
+//           MSP430F552x
+//         ---------------
+//     /|\|               |
+//      | |               |
+//      --|RST            |
+//        |               |
+//        |           P1.0|-->LED
+//
+//   Bhargavi Nisarga
+//   Texas Instruments Inc.
+//   April 2009
+//   Built with CCE v3.1 and IAR Embedded Workbench Version: 4.20
+//******************************************************************************
+
+#include  "msp430x552x.h"
+
+void main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  P1DIR |= 0x01;                            // P1.0 output
+  TA1CCTL0 = CCIE;                          // CCR0 interrupt enabled
+  TA1CCR0 = 1000-1;
+  TA1CTL = TASSEL_1 + MC_1 + TACLR;         // ACLK, upmode, clear TAR
+
+  __bis_SR_register(LPM3_bits + GIE);       // Enter LPM3, enable interrupts
+  __no_operation();                         // For debugger
+}
+
+// Timer1 A0 interrupt service routine
+#pragma vector=TIMER1_A0_VECTOR
+__interrupt void TIMER1_A0_ISR(void)
+{
+  P1OUT ^= 0x01;                            // Toggle P1.0
+}
+
---
+++ trunk/Docs/MSP430F5529 items/README.txt
@@ -0,0 +1,3 @@
+Go to the following website to get all of the needed software for this processor.
+
+http://focus.ti.com/docs/prod/folders/print/msp430f5529.html
