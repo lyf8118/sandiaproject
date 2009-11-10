f8a76a7cdce707c64a2313fddcc47a6adf1de8f9
c.shirts <c.shirts@291c8d24-a182-11de-89ee-f3da9a46d971>
2009-11-10 20:02:12 +0000

    Weekly report and final agenda (Agenda 10 November 2009.doc). Commented timer_ad_conv_LPM3_code.c

--- trunk/Test Programs/TestBoard/Timer/main.c
+++ trunk/Test Programs/TestBoard/Timer/main.c
@@ -9,21 +9,19 @@ int main( void )
   WDTCTL = WDTPW + WDTHOLD; 
  P1DIR |= 0x03;                            // P1.0 output
   CCTL0 = CCIE;                             // CCR0 interrupt enabled
-  CCR0 = 2000;//0xfff9;
-  BCSCTL1 |= 0x30;
-  // a/d conversion
+  CCR0 = 2000;//0xfff9;						// number of clock ticks till interrupt
+  BCSCTL1 |= 0x30;							//ACLK divider, /8
+  // a/d conversion, this slow ACLK might need to be sped up for conversion process
   ADC12CTL0 = SHT0_2 + ADC12ON;             // Set sampling time, turn on ADC12
-  ADC12CTL1 = SHP + 0x8;                          // Use sampling timer
-  ADC12IE = 0x01;                           // Enable interrupt
-  ADC12CTL0 |= ENC;
-  P6SEL |= 0x01;
-  
-  TACTL = TASSEL_1 + MC_1+ID_3;  
-  const char sr = LPM3_bits + GIE;
- // for(;;){
-    _BIS_SR(sr);                 // Enter LPM3 w/ interrupt
-    
-//  }
+  ADC12CTL1 = SHP + 0x8;               // Use sampling timer, select ACLK as source clock
+  ADC12IE = 0x01;                      // Enable interrupt
+  ADC12CTL0 |= ENC;						//Enable conversions
+  P6SEL |= 0x01;						//Use A/D for P6.0, will expand to use 
+											// P6.1 and P6.2 for y, z axis
+  TACTL = TASSEL_1 + MC_1+ID_3;			//For timer A, select ACLK source
+										//MC_1 = mode, ID_3 = divide source / 8
+  const char SR = LPM3_bits + GIE;
+  _BIS_SR(SR);                 // Enter LPM3 w/ interrupt
 }
 
 // Timer A0 interrupt service routine
@@ -31,9 +29,9 @@ int main( void )
 __interrupt void Timer_A (void)
 {
   P1OUT ^= 0x01;                            // Toggle P1.0
-  CCR0 += 2000;//0xfff9;                            // Add Offset to CCR0
-//  _BIC_SR_IRQ(LPM3_bits);                    // Clear LPM3
-  ADC12CTL0 |= ADC12SC;                   // Sampling open after wakeup
+  CCR0 += 2000;//0xfff9;            // Add Offset to CCR0, 2000 is ~4 seconds
+  ADC12CTL0 |= ADC12SC;             //Sampling open after wakeup, this starts conversion
+									//process, ADC12 ISR will be triggered when done
 }
 
 // ADC12 interrupt service routine
@@ -44,4 +42,5 @@ __interrupt void ADC12_ISR (void)
       P1OUT &= ~0x02;                       // Clear P1.0 LED off
     else
       P1OUT |= 0x02;                        // Set P1.0 LED on
+	//  _BIC_SR_IRQ(LPM3_bits);       //code to clear LPM3, use if movement is detected
 }
