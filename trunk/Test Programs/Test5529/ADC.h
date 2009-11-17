b00a71176e5acfb180f88ab4794bc4bb5768500b
c.shirts <c.shirts@291c8d24-a182-11de-89ee-f3da9a46d971>
2009-11-17 17:33:28 +0000

    ADC code so far.

--- trunk/Test Programs/Test5529/ADC.h
+++ trunk/Test Programs/Test5529/ADC.h
@@ -1,7 +1,7 @@
 #ifndef _ADC_H
 #define _ADC_H
 
-#define U_ADCWAIT 500
+#define U_ADCWAIT 0x7fff
 #define U_ADCSAMPLESNEEDED 16
 
 typedef struct{
@@ -9,5 +9,22 @@ typedef struct{
   volatile char min;
   volatile unsigned int Results[U_ADCSAMPLESNEEDED];
 }ADCResults;
+
+volatile ADCResults ADXL[3];
+volatile char ADCSamples = 0;
+volatile char ADCSampleNum = 0;
+volatile char ADCCounter = 0;
+
+void EnableADCInterrupt(){}
+void DisableADCInterrupt(){}
+void ADCOff(){
+  if((ADC12CTL0 & ADC12ON) == ADC12ON)
+    ADC12CTL0 ^= ADC12ON;
+}
+
+void ADCOn(){
+  if((ADC12CTL0 & ADC12ON) != ADC12ON)
+    ADC12CTL0 ^= ADC12ON;
+}
 
 #endif
