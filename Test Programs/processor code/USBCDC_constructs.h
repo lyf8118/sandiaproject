bced3a775855c1dba5d6afd0bf9a05324baf6a92
hartmandrector@hotmail.com <hartmandrector@hotmail.com@291c8d24-a182-11de-89ee-f3da9a46d971>
2010-02-10 09:44:24 +0000

    didn't work

--- Test Programs/processor code/USBCDC_constructs.h
+++
@@ -1,4 +0,0 @@
-BYTE sendData_waitTilDone(BYTE* dataBuf, WORD size, BYTE intfNum, ULONG ulTimeout);
-BYTE sendData_inBackground(BYTE* dataBuf, WORD size, BYTE intfNum, ULONG ulTimeout);
-BYTE receiveData_waitTilDone(BYTE* dataBuf, WORD size, BYTE intfNum, ULONG ulTimeout);
-WORD receiveDataInBuffer(BYTE*,WORD,BYTE);
